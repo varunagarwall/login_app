95182a17bec2691ed9ad6240c14ae8ce51a7b0b7
Master <varunaggarwal102@gmail.com>
2024-04-18 22:51:00 +0530

    adding a delete link and making changes in post controller

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post')
+const comment = require('../models/comment')
 
 module.exports.create = function(req, res){
     Post.create({
@@ -8,4 +9,23 @@ module.exports.create = function(req, res){
         if(err){console.log('error in creating a post'); return;}
         return res.redirect('back');
     });
+}
+
+module.exports.destroy = function(req,res){
+    console.log(req.params)
+    Post.findById(req.params.id,function(err,post){
+        if(post.user==req.user.id){
+            console.log(req.user.id)
+            console.log(post.user)
+
+            post.remove();
+
+            comment.deleteMany({post:req.params.id},function(err){
+                res.redirect('/users/profile');
+                        } );
+        }
+        else{
+            return res.redirect('back');
+        }
+    })
 }
--- routes/post.js
+++ routes/post.js
@@ -8,4 +8,6 @@ const passport=require('passport');
 
 
 router.post('/create',passport.checkAuthentication,postController.create);
+router.get('/destroy/:id',passport.checkAuthentication,postController.destroy);
+
 module.exports=router;
